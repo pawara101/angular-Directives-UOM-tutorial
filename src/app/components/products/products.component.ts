8c23b6f6d866797068a71719c2c81929ae6e37e4
pawara101 <80420033+pawara101@users.noreply.github.com>
2022-09-18 12:57:17 +0530

    2 files modified

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -13,12 +13,55 @@ export class ProductsComponent implements OnInit {
   public isLowInventory: any;
   public riceQty: any;
 
+  public isLowInventoryDhal: any;
+  public dhalQty: any;
   getRiceStorage() {
     this.riceQty = 350;
     if (this.riceQty < 200 && this.riceQty >= 50) {
       this.isLowInventory = true;
     }
-
     return this.riceQty;
+  }
+
+  getDhalStorage() {
+    this.dhalQty = 100;
+    if (this.dhalQty < 200 && this.dhalQty >= 50) {
+      this.isLowInventoryDhal = true;
+    }
+    return this.dhalQty;
+  }
+
+
+  public products = [{
+    'productId' : "001",
+    'productName' : "White Basmathi Rice",
+    'createdDate': "Jan 29, 2020",
+    'quantity': 100,
+    'unitPrice': "Rs. 400",
+    'productDescription':"White Basmathi Rice imported from pakistan"
+  },
+  { 
+  'productId' : "002",
+  'productName' : "Flour",
+  'createdDate': "Jan 29, 2020",
+  'quantity': 50,
+  'unitPrice': "Rs. 190",
+  'productDescription':"Super Fine Whole grain general Purpose flour"
+  },
+  
+  { 
+    'productId' : "003",
+    'productName' : "sugar",
+    'createdDate': "Jan 29, 2020",
+    'quantity': 1200,
+    'unitPrice': "Rs. 200",
+    'productDescription':"White sugar manufactured by Palwatte Factory"
+  },{
+    'productId' : "004",
+    'productName' : "Dhal",
+    'createdDate': "Jan 29, 2020",
+    'quantity': 10,
+    'unitPrice': "Rs. 200",
+    'productDescription':"Imported mysoor dhal from India"
   }
 }
