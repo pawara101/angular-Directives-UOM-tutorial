760db2e78a1755e1409af93d6cbe877120ce12b8
pawara101 <80420033+pawara101@users.noreply.github.com>
2022-09-18 12:41:03 +0530

    2 files modified

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -9,4 +9,16 @@ export class ProductsComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  public isLowInventory: any;
+  public riceQty: any;
+
+  getRiceStorage() {
+    this.riceQty = 350;
+    if (this.riceQty < 200 && this.riceQty >= 50) {
+      this.isLowInventory = true;
+    }
+
+    return this.riceQty;
+  }
 }
