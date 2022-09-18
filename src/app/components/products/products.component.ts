831131450f619b852e136bbd40307c71b74fcd43
pawara101 <80420033+pawara101@users.noreply.github.com>
2022-09-18 13:08:55 +0530

    2 files modified

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -31,37 +31,39 @@ export class ProductsComponent implements OnInit {
     return this.dhalQty;
   }
 
+  public products = [
+    {
+      productId: '001',
+      productName: 'White Basmathi Rice',
+      createdDate: 'Jan 29, 2020',
+      quantity: 100,
+      unitPrice: 'Rs. 400',
+      productDescription: 'White Basmathi Rice imported from pakistan',
+    },
+    {
+      productId: '002',
+      productName: 'Flour',
+      createdDate: 'Jan 29, 2020',
+      quantity: 50,
+      unitPrice: 'Rs. 190',
+      productDescription: 'Super Fine Whole grain general Purpose flour',
+    },
 
-  public products = [{
-    'productId' : "001",
-    'productName' : "White Basmathi Rice",
-    'createdDate': "Jan 29, 2020",
-    'quantity': 100,
-    'unitPrice': "Rs. 400",
-    'productDescription':"White Basmathi Rice imported from pakistan"
-  },
-  { 
-  'productId' : "002",
-  'productName' : "Flour",
-  'createdDate': "Jan 29, 2020",
-  'quantity': 50,
-  'unitPrice': "Rs. 190",
-  'productDescription':"Super Fine Whole grain general Purpose flour"
-  },
-  
-  { 
-    'productId' : "003",
-    'productName' : "sugar",
-    'createdDate': "Jan 29, 2020",
-    'quantity': 1200,
-    'unitPrice': "Rs. 200",
-    'productDescription':"White sugar manufactured by Palwatte Factory"
-  },{
-    'productId' : "004",
-    'productName' : "Dhal",
-    'createdDate': "Jan 29, 2020",
-    'quantity': 10,
-    'unitPrice': "Rs. 200",
-    'productDescription':"Imported mysoor dhal from India"
-  }
+    {
+      productId: '003',
+      productName: 'sugar',
+      createdDate: 'Jan 29, 2020',
+      quantity: 1200,
+      unitPrice: 'Rs. 200',
+      productDescription: 'White sugar manufactured by Palwatte Factory',
+    },
+    {
+      productId: '004',
+      productName: 'Dhal',
+      createdDate: 'Jan 29, 2020',
+      quantity: 10,
+      unitPrice: 'Rs. 200',
+      productDescription: 'Imported mysoor dhal from India',
+    },
+  ];
 }
